3b1925cc0ca6c5b2a214d87b4febd0bc41194ff3
nlilaramani <6720026+nlilaramani@users.noreply.github.com>
2021-12-05 16:06:44 -0600

    Update userdb.py

--- userdb.py
+++ userdb.py
@@ -21,4 +21,26 @@ def login(username,password):
         cursor.close()
         conn.close()
     return user
-
+def register(fname,lname,email,username,pwd):
+    conn=pg.connect(dsn)
+    print(fname,lname,email,username,pwd)
+    insert_cmd="""
+        INSERT INTO ecommerce.user(fname,lname,email,username,password)
+        values(%s,%s,%s,%s,%s)
+    """
+    cursor=conn.cursor()
+    result=False
+    try:
+        cursor.execute(insert_cmd,(fname,lname,email,username,pwd))
+        conn.commit()
+        result=True
+    except pg.Error as e:
+        print(e.pgerror)
+        print("Error inserting values")
+        conn.rollback()
+        result=False
+    finally:
+        cursor.close()
+        conn.close()
+    return result
+ 
