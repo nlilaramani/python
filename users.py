b2150368f3a0c7fc4c6ef0f0eb504984fba277fa
nlilaramani <6720026+nlilaramani@users.noreply.github.com>
2021-12-06 20:07:11 -0600

    Add files via upload

--- users.py
+++ users.py
@@ -1,7 +1,24 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+import userdb
 bp = Blueprint('users', __name__, url_prefix='/users')
-@bp.route('/register')
+@bp.route('/register', methods=['GET','POST'])
 def register_user():
-	return "register user"
+        if request.method=='GET':
+                return render_template("user_registration.html")
+        else:
+                result=userdb.register(request.form['fname'],request.form['lname'],request.form['email'],request.form['username'],request.form['password'])
+                if result:
+                        return "User successfully registered!, <a href='login'>Login</a>"
+                else:
+                        return 'Error registering user'
+@bp.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == "GET":
+        return render_template("login_form.html")
+    else:
+        uname=request.form['username']
+        pwd=request.form['password']
+        return uname+", "+pwd
+
